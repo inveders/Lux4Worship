f43dd6306dd1cfe000398835d1fda3c078031106
inveders <alexandra.gnimadi@gmail.com>
2019-09-03 21:26:48 +0200

    Configuring fragments and theirs XML files

--- app/src/main/java/com/inved/lux4worship/MainActivity.java
+++ app/src/main/java/com/inved/lux4worship/MainActivity.java
@@ -12,14 +12,15 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
-import androidx.viewpager.widget.ViewPager;
+import androidx.fragment.app.FragmentTransaction;
+
 
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
 import com.inved.lux4worship.base.BaseActivity;
+
+import java.util.ArrayList;
 
 public class MainActivity extends BaseActivity implements NavigationView.OnNavigationItemSelectedListener{
 
@@ -34,7 +35,9 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
 
     //Declaration for fragments
     BottomNavigationView bottomNavigationView;
-    ViewPager viewPager;
+
+    private ArrayList<String> arrayListFragment = new ArrayList<>();
+
 
     private static final int NUM_PAGES = 3;
 
@@ -52,11 +55,6 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
         //NavigationDrawer
         logout=findViewById(R.id.activity_main_drawer_logout);
 
-        //Viewpager
-        viewPager = findViewById(R.id.viewpager_fragment); //Init Viewpager
-        setupFm(getSupportFragmentManager(), viewPager); //Setup Fragment
-        viewPager.setCurrentItem(0); //Set Currrent Item When Activity Start
-        viewPager.setOnPageChangeListener(new PageChange()); //Listeners For Viewpager When Page Changed
 
         //Configuration navigation view header
         NavigationView mNavigationView = findViewById(R.id.activity_main_nav_view);
@@ -64,8 +62,15 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
         navWorshipTeam = mNavigationView.getHeaderView(0).findViewById(R.id.nav_header_worship_team);
         navProfileImage = mNavigationView.getHeaderView(0).findViewById(R.id.nav_header_profile_image);
 
+        this.initializationView();
         this.configureDrawerLayout();
         this.configureNavigationView();
+    }
+
+    private void initializationView() {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.content, TeamFragment.newInstance());
+        transaction.commit();
     }
 
     @Override
@@ -143,21 +148,25 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
             switch (item.getItemId()) {
                 case R.id.bottom_navigation_menu_team:
-                    viewPager.setCurrentItem(0);
-                    toolbar.setTitle(getString(R.string.toolbar_title_main_activity));
-                    return true;
+                    selectedFragment = TeamFragment.newInstance();
+                    toolbar.setTitle(getString(R.string.bottom_navigation_menu_team));
+                    break;
                 case R.id.bottom_navigation_menu_songs:
-                    viewPager.setCurrentItem(1);
-                    toolbar.setTitle(getString(R.string.toolbar_title_main_activity));
-                    return true;
+                    selectedFragment = SongsFragment.newInstance();
+                    toolbar.setTitle(getString(R.string.bottom_navigation_menu_songs));
+                    break;
                 case R.id.bottom_navigation_menu_notes:
-                    viewPager.setCurrentItem(2);
-                    toolbar.setTitle(getString(R.string.toolbar_title_main_activity));
-                    return true;
+                    selectedFragment = NotesFragment.newInstance();
+                    toolbar.setTitle(getString(R.string.bottom_navigation_menu_notes));
+                    break;
             }
-            return false;
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.activity_main_frame_layout, selectedFragment);
+            transaction.commit();
+            return true;
         }
     };
 
@@ -165,29 +174,8 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
     // NAVIGATION
     // -------------------
 
-    //Viewpager
 
 
-    public static void setupFm(FragmentManager fragmentManager, ViewPager viewPager){
-
-        FragmentPagerAdapter Adapter2 = new FragmentPagerAdapter(fragmentManager) {
-            @Override
-            public Fragment getItem(int position) {
-                return null ;
-            }
-
-            @Override
-            public int getCount() {
-                return NUM_PAGES;
-            }
-        };
-      /*  FragmentAdapter Adapter = new FragmentAdapter(fragmentManager);
-        //Add All Fragment To List
-        Adapter2..add(new TeamFragment(), "Page Team");
-        Adapter.add(new SongsFragment(), "Page Songs");
-        Adapter.add(new NotesFragment(), "Page Notes");
-        viewPager.setAdapter(Adapter);*/
-    }
 
 
 
@@ -228,26 +216,5 @@ public class MainActivity extends BaseActivity implements NavigationView.OnNavig
         };
     }*/
 
-    public class PageChange implements ViewPager.OnPageChangeListener {
-        @Override
-        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-        }
-        @Override
-        public void onPageSelected(int position) {
-            switch (position) {
-                case 0:
-                    bottomNavigationView.setSelectedItemId(R.id.bottom_navigation_menu_team);
-                    break;
-                case 1:
-                    bottomNavigationView.setSelectedItemId(R.id.bottom_navigation_menu_songs);
-                    break;
-                case 2:
-                    bottomNavigationView.setSelectedItemId(R.id.bottom_navigation_menu_notes);
-                    break;
-            }
-        }
-        @Override
-        public void onPageScrollStateChanged(int state) {
-        }
-    }
+
 }
--- app/src/main/java/com/inved/lux4worship/TeamFragment.java
+++ app/src/main/java/com/inved/lux4worship/TeamFragment.java
@@ -1,6 +1,26 @@
 package com.inved.lux4worship;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 public class TeamFragment extends Fragment {
+
+    public static TeamFragment newInstance() {
+        TeamFragment fragment = new TeamFragment();
+        return fragment;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_team, container, false);
+      //  unbinder = ButterKnife.bind(this, rootView);
+        return rootView;
+    }
 }
